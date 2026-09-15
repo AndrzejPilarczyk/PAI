280d68bb281f5ba4e9c6dd04e4175cb5679e9c00
Andrzej Pilarczyk <apilarczyk@technikumlodz.pl>
2026-09-15 13:21:53 +0200

    5 zadan z lekcji

---
+++ zadanie 4 - PHP/index.php
@@ -0,0 +1,60 @@
+<?php
+    //zad1
+    /*
+    $x = 3;
+
+    if($x % 2 == 0){
+        echo "liczba jest parzysta";
+    } else {
+        echo "liczba nie jest parzysta";
+    }
+        */
+
+    //zad2
+    /*
+    $a = 4;
+    $b = 2;
+
+    if($a % $b == 0){
+        echo "liczba jest podzielna";
+    } else {
+        echo "liczba nie jest podzielna";
+    }
+        */
+
+    //zad 3
+    /*
+    $c = 17;
+
+    if($c >= 1 && $c <= 10){
+        echo "liczba należy do przedziału od 1 do 10";
+    } else if ($c >= 17 && $c <= 21){
+        echo "liczba należy do przedziału od 17 do 21";
+    } else {
+        echo "liczba nie należy do żadnego przedziału liczb";
+    }
+        */
+
+    //zad4
+    /*
+    $d = -1;
+
+    if($d > 0){
+        echo "liczba jest większa od zero";
+    } else if ($d < 0){
+        echo "liczba jest mniejsza od zero";
+    } else {
+        echo "liczba jest równa zero";
+    }
+        */
+
+    //zad5
+    $e = 18;
+    if($e < 11){
+        echo "Dziecko";
+    } else if ($e >= 11 && $e <= 17){
+        echo "nastolatek";
+    }else{
+        echo "dorosły";
+    }
+?>
